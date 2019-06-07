fdfc028c1e7e1420b1bacbf870d42c5d5d0a688b
liam-cb <ldyer@thecitybase.com>
2019-06-06 19:24:33 -0600

    Make a GQL request

--- src/Main.purs
+++ src/Main.purs
@@ -2,24 +2,20 @@ module Main where
 
 import Prelude
 
+import Affjax (ResponseFormatError, URL, request)
+import Affjax.RequestBody as RequestBody
+import Affjax.RequestHeader (RequestHeader(..))
+import Affjax.ResponseFormat as ResponseFormat
+import Data.Argonaut.Core (Json)
+import Data.Either (Either(..))
+import Data.HTTP.Method (Method(..))
+import Data.Maybe (Maybe(..))
+import Data.MediaType (MediaType(..))
 import Data.String (joinWith)
 import Effect (Effect)
-import Effect.Aff (Aff, Error)
+import Effect.Aff (Aff, launchAff_)
 import Effect.Class.Console (log)
-
-{-
-
-  query {
-    person {
-      name
-      age
-    }
-    business {
-     address
-    }
-  }
-
--}
+import Simple.JSON (writeJSON)
 
 newtype FieldName = FieldName String
 newtype Fields a = Fields (Array a)
@@ -39,13 +35,37 @@ instance gqlSerializeFields :: (GQLSerialize a) => GQLSerialize (Fields a) where
 instance gqlSerializeSelectionSet :: GQLSerialize SelectionSet where
   toGQL (SelectionSet fn fs) = (toGQL fn) <> (toGQL fs)
 
-test :: SelectionSet
-test = SelectionSet
+testSet :: SelectionSet
+testSet = SelectionSet
   (FieldName "query")
   (Fields [SelectionSet
             (FieldName "person")
             (Fields [SelectionSet (FieldName "name") (Fields [])])])
 
+gqlHubQuery :: SelectionSet
+gqlHubQuery = SelectionSet
+  (FieldName "query")
+  (Fields [ SelectionSet (FieldName "graphQLHub") (Fields []) ])
+
+--post :: forall a. ResponseFormat a -> URL -> RequestBody -> Aff (Response (Either ResponseFormatError a))
+
+executeGQL :: URL -> SelectionSet -> Aff (Either ResponseFormatError Json)
+executeGQL url selectionSet = do
+  response <- request
+    { method: Left POST
+    , url
+    , headers: [ ContentType (MediaType "application/json") ]
+    , content: Just $ RequestBody.string (writeJSON { query: toGQL selectionSet })
+    , username: Nothing
+    , password: Nothing
+    , withCredentials: false
+    , responseFormat: ResponseFormat.json
+    }
+  pure response.body
+
 main :: Effect Unit
-main = do
-  log $ toGQL test
+main = launchAff_ $ do
+  res <- executeGQL "https://www.graphqlhub.com/graphql" gqlHubQuery
+  case res of
+    Left _ -> log "uh oh"
+    Right _ -> log "cool"
