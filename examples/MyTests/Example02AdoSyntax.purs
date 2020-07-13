a5ea160fe740f40dd659ff38c39012220a26fa7a
Serhii Khoma <srghma@gmail.com>
2020-07-13 16:50:57 +0300

    feat: update

---
+++ examples/MyTests/Example02AdoSyntax.purs
@@ -0,0 +1,41 @@
+module Examples.MyTests.Example02AdoSyntax where
+
+import Protolude
+
+import Test.Spec as Test.Spec
+import Test.Spec.Assertions as Test.Spec
+import GraphqlClient
+import Swapi.Scalar
+import Swapi.Query as Query
+import Swapi.Interface.Character as Character
+
+type Response = CharacterResponse
+
+type CharacterResponse =
+  { name :: String
+  , id :: Id
+  , friends :: Array String
+  }
+
+query :: SelectionSet Scope__RootQuery Response
+query = Query.hero defaultInput characterInfoSelection
+
+characterInfoSelection :: SelectionSet Character.Scope__Character CharacterResponse
+characterInfoSelection = ado
+  name' <- Character.name
+  id' <- Character.id
+  friends' <- Character.friends Character.name
+  in { name: name', id: id', friends: friends' }
+
+expectedQuery :: String
+expectedQuery = """query { hero { name id friends { name } } }"""
+
+spec :: Test.Spec.Spec Unit
+spec = Test.Spec.it "Example02AdoSyntax" do
+  writeGraphql query `Test.Spec.shouldEqual` expectedQuery
+
+  (response :: Either (GraphqlError Response) Response) <- gqlRequest "https://elm-graphql.herokuapp.com" query
+
+  (response' :: Response) <- (throwError <<< error <<< printGraphqlError) \/ pure $ response
+
+  response' `Test.Spec.shouldEqual` { friends: ["Han Solo","Leia Organa","C-3PO","R2-D2"], id: Id "1000", name: "Luke Skywalker" }
