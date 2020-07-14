8708cba3221d67452ab33a4ba14eaf4fc066a11c
Serhii Khoma <srghma@gmail.com>
2020-07-14 13:52:46 +0300

    feat: update

--- examples/MyTests/Example05InterfacesAndUnions.purs
+++ examples/MyTests/Example05InterfacesAndUnions.purs
@@ -51,13 +51,3 @@ expectedQuery = inlineAndTrim """
 spec :: Test.Spec.Spec Unit
 spec = Test.Spec.it "Example05InterfacesAndUnions" do
   writeGraphql query `Test.Spec.shouldEqual` expectedQuery
-
-  (response :: Either (GraphqlError Response) Response) <- gqlRequest "https://elm-graphql.herokuapp.com" query
-
-  case response of
-       Left (GraphqlUserError detail (ParsedData Nothing)) ->
-         case NonEmptyArray.uncons detail of
-              { head: GraphqlUserErrorDetail { message }, tail: [] } -> message `Test.Spec.shouldEqual` "Artificial error..."
-              _ -> Test.Spec.fail "should be only 1"
-       Left _ -> Test.Spec.fail "should be GraphqlUserError"
-       Right _ -> Test.Spec.fail "should fail"
