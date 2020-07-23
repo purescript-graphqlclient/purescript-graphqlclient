bf6ed7b040fa605508de6b532444231f22c7cd20
Serhii Khoma <srghma@gmail.com>
2020-07-23 21:20:36 +0300

    feat: travis -> add retry

--- check-there-are-no-changed-files.sh
+++ check-there-are-no-changed-files.sh
@@ -2,7 +2,27 @@
 
 set -euxo pipefail
 
-yarn run test
+function retry()
+{
+        local n=0
+        local try=$1
+        local cmd="${@: 2}"
+        [[ $# -le 1 ]] && {
+        echo "Usage $0 <retry_number> <Command>"; }
+
+        until [[ $n -ge $try ]]
+        do
+                $cmd && break || {
+                        echo "Command Fail.."
+                        ((n++))
+                        echo "retry $n ::"
+                        sleep 1;
+                        }
+
+        done
+}
+
+retry 1 yarn run test
 
 ./regenerate-examples.sh
 
@@ -16,6 +36,6 @@ if [[ -n $changed_files ]]; then
   exit 1;
 fi
 
-yarn run examples:test
+retry 1 yarn run examples:test
 
 echo 'SUCCESS'
